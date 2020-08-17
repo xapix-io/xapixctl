9eb7d646a96753a22b89834118975e2e10fd7807
Michael Reinsch <michael@xapix.io>
2020-08-17 17:29:13 +0200

    updated help text

--- lib/xapixctl/preview_cli.rb
+++ lib/xapixctl/preview_cli.rb
@@ -9,14 +9,14 @@ module Xapixctl
     option :format, aliases: "-f", default: 'text', enum: ['text', 'yaml', 'json'], desc: "Output format"
     desc "pipeline ID", "Preview a pipeline"
     long_desc <<-LONGDESC
-      `xapixctl preview` will return a preview of the given pipeline.
+      `xapixctl preview pipeline` will return a preview of the given pipeline.
 
       The preview function will not call any external data sources but calculate a preview based on the provided sample data.
 
-      To preview a pipeline attached to an endpoint, please use `xapixctl preview-ep` to see the correct preview.
+      To preview a pipeline attached to an endpoint, please use `xapixctl preview endpoint` to see the correct preview.
 
       Examples:
-      \x5> $ xapixctl preview -o xapix -p some-project pipeline
+      \x5> $ xapixctl preview pipeline -o xapix -p some-project pipeline
     LONGDESC
     def pipeline(pipeline)
       connection.pipeline_preview(pipeline, org: options[:org], project: options[:project], format: options[:format].to_sym) do |res|
@@ -30,12 +30,12 @@ module Xapixctl
     option :format, aliases: "-f", default: 'text', enum: ['text', 'yaml', 'json'], desc: "Output format"
     desc "endpoint ID", "Preview an endpoint"
     long_desc <<-LONGDESC
-      `xapixctl preview-ep` will return a preview of the given endpoint.
+      `xapixctl preview endpoint` will return a preview of the given endpoint.
 
       The preview function will not call any external data sources but calculate a preview based on the provided sample data.
 
       Examples:
-      \x5> $ xapixctl preview-ep -o xapix -p some-project endpoint
+      \x5> $ xapixctl preview endpoint -o xapix -p some-project endpoint
     LONGDESC
     def endpoint(endpoint)
       connection.endpoint_preview(endpoint, org: options[:org], project: options[:project], format: options[:format].to_sym) do |res|
